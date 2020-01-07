dcf948adcdddadc57d1a48620db6d9da1a11cf08
Paloma van Moerkerken <Paloma@wcw-stud-145-109-29-186.wireless.uva.nl>
2020-01-07 12:57:56 +0100

    laden van cars is af

--- rushhour.py
+++ rushhour.py
@@ -7,3 +7,26 @@
 class Rushhour():
     
     def __init__(self, game):
+        data = self.load_games(game)
+
+        return 
+
+    def load_games(self, filename):
+
+        # list for returning
+        games_data = []
+
+        # load from datafile
+        with open(filename, "r") as f:
+            game_data = []
+            
+            for line in f:
+                line = line.split()
+                game_data.append(line)
+            game_data.remove(game_data[0])                                
+        return game_data
+    
+     
+if __name__ == "__main__":
+    rushhour = Rushhour("games/Rushhour6x6_1.csv")
+  
