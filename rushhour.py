1ca37907aa7266630ad5c6bb1fef118dad5de3cd
Paloma van Moerkerken <Paloma@wcw-stud-145-109-21-1.wireless.uva.nl>
2020-01-08 09:51:55 +0100

    strippen

--- rushhour.py
+++ rushhour.py
@@ -32,9 +32,10 @@ class Rushhour():
     """ loads rooms from filename in two-step process """
     def car_objects(self, data):
 
-        # phase 1: creates room objects for each set of data we just parsed
+        # creates car objects for each set of data
         i = 0
         for car_data in data:            
+            
             # creates identifier
             i += 1
             id = i
@@ -42,33 +43,13 @@ class Rushhour():
             # creates the object with id, name, orientation, coordinates and length
             new_car = Car(id, car_data[0], car_data[1], car_data[2], car_data[3])
             self.cars[id] = new_car
-            
-        # # strip the strings
-#         for string in self.cars[id]:
-#             print(string.items)
-#
         
-    
-
-        # phase 2: links rooms to each other
-        for car in self.cars.values():
-            print(car.name)
-
-            # # creates identifier
-#                id = int(room_data[0])
-
-            # retrieves exisiting room object from dictionary
-            car = self.cars[id]
             
+        for car in self.cars.values():
+            # name of car
+            car_name = car.name
+            car_name = car_name.strip(',')
 
-            # # extracts the connection data
-#                 connections = room_data[4:]
-
-            # # adds connections to the room
-#                 for connection in connections:
-#                     direction, target_room_id = connection.split()
-#
-#                     room.add_connection(direction, target_room_id)
         return
 
  
