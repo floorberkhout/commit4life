c2b8ca604fe6fc9b58c651e25e6bbc582d545126
Paloma van Moerkerken <Paloma@wcw-stud-145-109-30-134.wireless.uva.nl>
2020-01-14 13:28:40 +0100

    kleine letters gemaakt

--- code/algoritmes/palomaalgoritme.py
+++ code/algoritmes/palomaalgoritme.py
@@ -1,55 +1,22 @@
 import time
 from board import Board
 
-def Algoritme1(board):    
-    game_won = False
-    log_file = "resultaten/log.csv"
-    log = open(log_file, "w")
-    log.truncate()
-    start = time.time()
-    header = "car" + ',' + "move" + '\n'
-    log.write(header)
-    board.print_board()
-    move_count = 0
+def algoritme1(board):    
     
     # Plays the game untill won
-    while game_won == False:
-        # end_board = board.board[board.length-1][int(board.length/2-0.5)]
-#         if end_board != ".":
+    while board.game_won == False:
         for col in board.board:
             print(col)
             for car in col:
                 if car != ".":
                     print(type(car))
-                    # for car.orientation
-                    
-#                 else:
-#                     print("nee")
-                # if board[x+position+request_move][y] != "." and board.board[x+position+request_move][y] != request_car.name or x + position + request_move < 0:
-#
-            
-            
-        
-        
-    # Get random car and move
-            
-        # request_car = random.choice(list(board.cars.values()))
-#
-        #request_move = random.choice([-1, 1])
-    
-        # move(board, request_car, request_move)
-#         write_move(request_car, request_move, log)
-
-        # Increases move count
-            move_count += 1
+  
+            board.move_count += 1
     
         # check if the game has been won ( when the XX car is in front of the exit)
         if board.board[board.length-1][int(board.length/2-0.5)] == "X":
             time_elapsed = time.time() - start
             game_won = True
             
-            # print the board one more time and tell the player he has won
-            print("Congratulations you've won the game!")
-            print("Move count: ", move_count)
-            print("Time elapsed: ", time_elapsed)
-    return move_count, time_elapsed
+         
+    return board.move_count, time_elapsed
--- main.py
+++ main.py
@@ -7,36 +7,17 @@ sys.path.append(os.path.join(directory, "code", "algoritmes"))
 
 # importeer de gebruikte structuur
 from board import Board
-<<<<<<< HEAD
-from random_move import Random_move
+from random_algo import random_algo
+from winning_row import winning_row
+from palomaalgoritme import algoritme1
 
-from winning_row import Winning_row
-
-
-def main():
-    board = Board("data/Rushhour6x6_1.csv")
-    
-    result = Winning_row(board)
-
-    # result = Random_move(board)
-
-
-=======
-from random_move import random_algo
-
-from winning_row import Winning_row
-from palomaalgoritme import Algoritme1
-
-
-# from code.classes import car, board
-# from code.algoritmes import random_move
 
 def main():
 
     board = Board("data/Rushhour6x6_1.csv")
     
     # random algo
-    move_count, time_elapsed = random_algo(board)
+    # move_count, time_elapsed = random_algo(board)
     
     board.print_board()
     board.end_game(move_count, time_elapsed)
@@ -45,9 +26,6 @@ def main():
     # result = Winning_row(board)
     
     # palomaalg = Algoritme1(board)
-
->>>>>>> 5ef47d070d3fde8d92aec0bf1657060f722c0f02
-
 
 if __name__ == "__main__":
     main()
