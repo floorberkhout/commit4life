1f2e93c046d6339080f21296c86bdbfad91cb1a4
Paloma van Moerkerken <Paloma@wcw-stud-145-109-21-121.wireless.uva.nl>
2020-01-15 14:20:34 +0100

    hliefeg

--- code/algoritmes/random_algo.py
+++ code/algoritmes/random_algo.py
@@ -14,7 +14,6 @@ def random_algo(board):
         
         board.move(request_car, request_move)
         board.write_move(request_car, request_move, board.log)
-
         
         # check if the game has been won ( when the XX car is in front of the exit)
         if board.board[board.length-1][int(board.length/2-0.5)] == "X":
