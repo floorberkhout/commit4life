185fa95f397a7777aa9564ace9a23e47679cd9eb
Paloma van Moerkerken <Paloma@wcw-stud-145-109-42-156.wireless.uva.nl>
2020-01-17 14:24:42 +0100

    improved random function, eindelijk heb ik de beweefgbare auto's gevonden jeetje

--- main.py
+++ main.py
@@ -9,46 +9,20 @@ import numpy as np
 from board import Board
 from random_algo import random_algo
 from winning_row import winning_row
-<<<<<<< HEAD
 from improved_random import algoritme1
-=======
 from breath_first import breath_first
 from depth_first import depth_first
->>>>>>> bff683be3c653ef7dfe42aa575df54256b01ee82
 
-# def main():
-#
-#     board = Board("data/Rushhour6x6_3.csv")
-#
-#     # move_count, time_elapsed = winning_row(board)
-#
-#     move_count, time_elapsed = breath_first(board)
-#
-#     board.print_board()
-
-<<<<<<< HEAD
-def main():
-
-    board = Board("data/Rushhour6x6_1.csv")
-    
-    # random algo
-    # move_count, time_elapsed = winning_row(board)
-    
-
-    #move_count, time_elapsed = random_algo(board)
-
-    improved_random = algoritme1(board)
-=======
->>>>>>> bff683be3c653ef7dfe42aa575df54256b01ee82
 
 def main():
     """ Runs Rush Hour game with the algorithm """
     
     # Creates board
-    board = Board("data/Rushhour6x6_1.csv")
+    board = Board("data/Rushhour6x6_3.csv")
    
     # Runs algorithm
-    move_count, time_elapsed = depth_first(board)
+    # move_count, time_elapsed = depth_first(board)
+    improved_random = algoritme1(board)
 
     # Prints results
     board.print_board()
