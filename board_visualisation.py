6e51b399a7888658e8675452a32c2fe5a7ff8458
floorberkhout <floorberkohut0@gmail.com>
2020-01-21 17:16:50 +0100

    board visualisation by steps as input

--- board_visualisation.py
+++ board_visualisation.py
@@ -4,20 +4,11 @@ sys.path.append(os.path.join(directory, "code"))
 sys.path.append(os.path.join(directory, "code", "classes"))
 sys.path.append(os.path.join(directory, "code", "algoritmes"))
 import numpy as np
-import random
-
-import time
-from IPython import display
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from matplotlib import colors
 
 # importeer de gebruikte structuur
 from board import Board
-from random_algo import random_algo
-from breath_first2 import breath_first
-from depth_first import depth_first
-from improved_random import algoritme1
 
 def create_numpy(board, my_board):
     """ Creates a numpy board """
@@ -35,16 +26,18 @@ def create_numpy(board, my_board):
 
 def visualize_board(steps):
     """ Animates rush hour game 6x6_1 """
+    
     count = 0
     cmap = colors.ListedColormap(['white', 'grey', 'green', 'blue', 'pink', 'orange', 'black','purple', 'brown', 'beige', 'yellow', 'turquoise', 'coral', 'red'])
     
-    # Creates board
+    # Creates board object
     board = Board("data/Rushhour6x6_1.csv")
     
-    # Creates numpy board
+    # Creates begin state
     my_board = np.zeros((board.length, board.length))
     my_board = create_numpy(board, my_board)
     
+    # Creates state created by each step
     for step in steps:
         for car in board.cars.values():
             if car.name == step[0]:
@@ -52,15 +45,15 @@ def visualize_board(steps):
                 board.move(request_car, step[1])
                 my_board = create_numpy(board, my_board)
         
+        # Visualizes numpy list
         fig = plt.gcf()
         im = plt.imshow(my_board, cmap=cmap, animated=True)              
-
-        plt.savefig(fname=f'{count}game_of_life', dpi=150)
+        
+        # Saves visualisation
+        plt.savefig(fname=f'{count}Rush_hour6x6_1', dpi=150)
     
         count += 1
         
-    
-
 if __name__ == "__main__":
     steps = [["A", -1], ["B", -1]]
     visualize_board(steps)
