05f797a34f63cda787ee173b9ce7f48b10dd64d2
floorberkhout <floorberkohut0@gmail.com>
2020-01-21 16:50:45 +0100

    board visualisation by steps as input

--- board_visualisation.py
+++ board_visualisation.py
@@ -19,65 +19,45 @@ from breath_first2 import breath_first
 from depth_first import depth_first
 from improved_random import algoritme1
 
-
-def init_board(board, my_board, request_car, request_move):
-    """ Initialize the board with starting positions """
+def visualize_game(board, my_board):
+    """ Creates a numpy board """
     
-    count = 1
-    global cars 
-    cars = {}
-    for car in board.cars.values():
-        y = car.coordinates[0] 
-        x = car.coordinates[1]       
-        move = 0
-        if car.name == request_car:
-            move = request_move
-        # Saves coordinates of cars in list board
-        if(car.orientation=="V"):
-            for letter in range(car.length):
-                my_board[[x-letter+move], [y]] = count 
-                cars[car.name] = count
-                print(my_board)
-        else:
-            for letter in range(car.length):
-                my_board[[x], [y+letter+move]] = count
-                cars[car.name] = count
-        count += 1
-
+    for i in range(len(board.board)):
+        for n in range(len(board.board)):
+            if board.board[n][i] == ".":
+                my_board[[i], [n]] = 0
+            elif board.board[n][i] == "X":
+                my_board[[i], [n]] = 2
+            else:
+                my_board[[i], [n]] = 1
     return my_board
 
-def update_board(my_board):
-    """ Update the board based on the game rules, each call to update_board is one turn """
-    old_board = my_board.copy()
-    
-    return my_board
 
 def main():
     """ Animates rush hour game 6x6_1 """
-    request_car = ""
-    request_move = 0
     count = 0
+    steps = [["A", -1], ["B", -1]]
     cmap = colors.ListedColormap(['white', 'grey', 'green', 'blue', 'pink', 'orange', 'black','purple', 'brown', 'beige', 'yellow', 'turquoise', 'coral', 'red'])
     
     # Creates board
     board = Board("data/Rushhour6x6_1.csv")
     
-    # Input variables for the board
-    boardsize = board.length               
+    # Creates numpy board
+    my_board = np.zeros((board.length, board.length))
+    my_board = visualize_game(board, my_board)
     
-    my_board = np.zeros((boardsize,boardsize))
-    
-    for i in range(2):
-        print(my_board)
-        my_board = init_board(board, my_board, request_car, request_move)
-        print(my_board)
-        request_car = "A"
-        request_move = -1
+    for step in steps:
+        for car in board.cars.values():
+            if car.name == step[0]:
+                request_car = car
+                board.move(request_car, step[1])
+                my_board = visualize_game(board, my_board)
         
         fig = plt.gcf()
         im = plt.imshow(my_board, cmap=cmap, animated=True)              
 
         plt.savefig(fname=f'{count}game_of_life', dpi=150)
+    
         count += 1
         
     
