61e9aefccb407461af85c81dcf3f9c807879a310
Bart Zeeuw van der Laan <Bart_Zeeuw_van_der_Laan@mckinsey.com>
2020-01-22 10:29:31 +0100

    updated depth and breath first

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 
 # importeer de gebruikte structuur
 from board import Board
+from csvwriter import CsvWriter
 # from random_algo import random_algo
 from winning_row import winning_row
 from breath_first2 import breath_first
@@ -14,23 +15,38 @@ from depth_first import depth_first
 from node import Node
 from improved_random import algoritme1
 
-
 def main():
     """ Runs Rush Hour game with the algorithm """
 
+    # selectors
+    algorithm = "breath_first"
+    memory_clearer = True
+
+    x = algorithm[:-6]
+    if memory_clearer:
+        algorithm = algorithm + "_memory_clearer"
+
     # Creates board
-    board = Board("data/Rushhour9x9_4.csv")
+    board = Board("data/Rushhour6x6_2.csv")
 
     # Runs algorithm
     name = (0,)
     first_node = Node(board, name)
 
-    breath_first_algorithm = breath_first(first_node)
-    solution = breath_first_algorithm.run()
+
+
+    x_first_algorithm = breath_first(first_node, memory_clearer, x)
+    solution, time_elapsed = x_first_algorithm.run()
+    time_elapsed = round(time_elapsed, 2)
 
     # Prints results
-    print(len(solution.history))
-    print(solution.history)
+    move_count = len(solution)
+    print(solution)
+    print("Move count:", move_count)
+    print("Time elapsed: ", time_elapsed)
+
+    writer = CsvWriter(algorithm, board.name)
+    writer.write_to_csv(time_elapsed, board.name, algorithm, move_count, solution)
 
 if __name__ == "__main__":
     main()
