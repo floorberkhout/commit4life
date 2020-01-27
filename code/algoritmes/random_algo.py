a1c2889c0eec15ef6ebf268516e541317d9f54d4
floorberkhout <floorberkohut0@gmail.com>
2020-01-27 10:53:56 +0100

    commented random

--- code/algoritmes/random_algo.py
+++ code/algoritmes/random_algo.py
@@ -7,26 +7,17 @@ def random_algo(board):
     
     # Plays the game untill won
     while board.game_won == False:
-        # Get random car and move
-        request_car = random.choice(list(board.cars.values()))
         
+        # Gets random car and move
+        request_car = random.choice(list(board.cars.values()))  
         request_move = random.choice([-1, 1]) 
         
-<<<<<<< HEAD
-        board.move(request_car, request_move)
-        
-        solution = board.move_count
-=======
+        # Moves car and if moved adds step to solution
         moved = board.move(request_car, request_move)
         if moved is not 0:
             solution.append(", ".join([request_car.name, str(request_move)]))
->>>>>>> 7f46a9917aca824be549b78de23fd705bd87c9a5
 
         # Checks if another car prevents the winning car from getting out
         board.game_won, time_elapsed = board.check_win(board.start)
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> 7f46a9917aca824be549b78de23fd705bd87c9a5
     return solution, time_elapsed
--- main.py
+++ main.py
@@ -22,32 +22,20 @@ from board_visualisation import visualize_board
 def main():
     """ Runs Rush Hour game with the algorithm """
 
-    # # Creates board
-<<<<<<< HEAD
+    # Creates board
     board = Board("data/Rushhour12x12_7.csv")
-=======
-<<<<<<< HEAD
-    board = Board("data/Rushhour9x9_5.csv")
->>>>>>> 0fc4d73ce93a30ab48f71d61401be891c5d6b554
 
-    """
-    Selectors choose between all the algorithms and depth first and choose whether _memory_clearer = True or False
-    """
+
+    # Choose between all the algorithms and depth first and choose whether _memory_clearer = True or False 
+    
     # algorithm = "random_algo"
     # algorithm = "improved_random"
-<<<<<<< HEAD
     # algorithm = "breadth_first"
     algorithm = "depth_first"
+    
     memory_clearer = False
 
     x = algorithm
-=======
-#     algorithm = "breadth_first"
-    algorithm = "depth_first"
-    memory_clearer = True
-
-    x = algorithm
-    
     if memory_clearer:
         algorithm = algorithm + "_memory_clearer"
 
@@ -55,51 +43,12 @@ def main():
     first_node_name = (0,)
     first_node = Node(board, first_node_name)
  
-    # Setup and run the algorithm
+    # Sets and runs the algorithm
     x_first_algorithm = X_first(first_node, memory_clearer, x) 
     
     if x == "depth_first" or x == "breadth_first":    
         solution, time_elapsed, nodes_dict = x_first_algorithm.run()
     
-    elif x == "random_algo":
-        solution, time_elapsed = random_algo(board)
-=======
-    board = Board("data/Rushhour6x6_1.csv")
-
-
-    solution, time_elapsed = improved_random(board)
-    
-    board.end_game(solution, time_elapsed)
- 
- 
- 
- 
- 
-    #
-    # Runs algorithm
-    # move_count, time_elapsed, nodes_list = depth_first(board)
-
-
-
-
-    # Selectors choose between breadth and depth first and choose whether _memory_clearer = True or False
-    
->>>>>>> 0fc4d73ce93a30ab48f71d61401be891c5d6b554
-    
-    if memory_clearer:
-        algorithm = algorithm + "_memory_clearer"
-
-    # Initializes the first node
-    first_node_name = (0,)
-    first_node = Node(board, first_node_name)
- 
-    # Setup and run the algorithm
-    x_first_algorithm = X_first(first_node, memory_clearer, x) 
-    
-    if x == "depth_first" or x == "breadth_first":    
-        solution, time_elapsed, nodes_dict = x_first_algorithm.run()
-    
-<<<<<<< HEAD
     elif x == "random_algo":
         solution, time_elapsed = random_algo(board)
     
@@ -108,30 +57,9 @@ def main():
         
     board.end_game(solution, time_elapsed)
 
-    # write the solution to a CSV file
+    # Writes the solution to a CSV file
     writer = CsvWriter(algorithm, board.name)
     writer.write_to_csv(time_elapsed, board.name, algorithm, solution)
-=======
-    # print(solution)
-    # print("Move count:", move_count)
-    # print("Time elapsed: ", time_elapsed)
->>>>>>> 7f46a9917aca824be549b78de23fd705bd87c9a5
-    
-    elif x == "improved_random":
-        solution, time_elapsed = improved_random(board)
-
-        
-    board.end_game(move_count, time_elapsed)
-
-    # write the solution to a CSV file
-<<<<<<< HEAD
-    writer = CsvWriter(algorithm, board.name)
-    writer.write_to_csv(time_elapsed, board.name, algorithm, move_count, solution)
-=======
-    # writer = CsvWriter(algorithm, board.name)
-#     writer.write_to_csv(time_elapsed, board.name, algorithm, move_count, solution)
->>>>>>> 7f46a9917aca824be549b78de23fd705bd87c9a5
->>>>>>> 0fc4d73ce93a30ab48f71d61401be891c5d6b554
 
 
     
